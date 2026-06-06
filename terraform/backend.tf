11dfe58d651f80bea61b037c9ee137aafb6f1adf
Akshai S <asurendran@ecloudcontrol.com>
2026-06-06 14:24:34 +0530

    Refactor Terraform configuration to use local backend and simplify provider settings

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket         = "devops-assignment-tfstate"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "devops-assignment-tfstate-lock"
-    encrypt        = true
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
